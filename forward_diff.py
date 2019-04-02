55adb1774e77849e0e2c57b63e7fad1f1f51b892
Deshana Desai <desai.deshna@gmail.com>
2019-04-02 17:23:51 -0400

    update main

--- forward_diff.py
+++ forward_diff.py
@@ -3,15 +3,54 @@ import sys
 
 from pycparser import parse_file
 
+def differentiate(node):
+    # print(show_attrs(node.__class__))
+    if node.__class__.__name__ == 'BinaryOp':
+        op = node.op
+        if (op)=='+':
+            print("x=a+b")
+        elif (op) == '*':
+            print("Mult")
+        else:
+            print(op)
 
-def grad(fun, x=0):
-	"""
-	Returns a function which computes gradient of `fun` with respect to
-	positional argument number `x`. The returned function takes the 
-	same arguments as `fun` , but returns the gradient instead. The function
-	`fun` is expected to be scalar valued. The gradient has the same type as argument."""
-	assert type(x) in (int, tuple, list), x
-	return get_traversal(fun,x)
+    elif node.__class__.__name__ == 'FuncCall':
+        print(node.name.name)
+    return
+
+def show_attrs(node):    
+    for attr in dir(node):
+        print("-----------------")
+        print(attr)
+        print(getattr(node,attr))
+
+def get_traversal(fun,x):
+    nodes = []
+    stack = [fun]
+    while stack:
+        cur_node = stack[0]
+
+        differentiate(cur_node)
+        stack = stack[1:]
+        nodes.append(cur_node)
+
+        if 'left' in dir(cur_node):
+            stack.append(cur_node.left)
+        if 'right' in dir(cur_node):
+            stack.append(cur_node.right)
+    return 0
+
+
+def grad(ast, x=0):
+    """
+    Returns a function which computes gradient of `fun` with respect to
+    positional argument number `x`. The returned function takes the 
+    same arguments as `fun` , but returns the gradient instead. The function
+    `fun` is expected to be scalar valued. The gradient has the same type as argument."""
+    assert type(x) in (int, tuple, list), x
+    fun = ast.ext[-1].body.block_items[1].init
+    fun.show()
+    return get_traversal(fun,x)
 
 
 if __name__ == "__main__":
@@ -23,5 +62,5 @@ if __name__ == "__main__":
     ast = parse_file(filename, use_cpp=True,
             cpp_path='gcc',
             cpp_args=['-E', r'-Iutils/fake_libc_include'])
-    ast.show()
-    # grad(ast)
+    # ast.show()
+    grad(ast)
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Readme
+
+# To do
+
+* Expressions with parenthesis traversal
