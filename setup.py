d8334c706bc64aa969d3e5cd459a42bd17d1f9cf
deshanadesai <desai.deshna@gmail.com>
2020-04-12 19:59:57 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autodiff-deshanadesai", # Replace with your own username
+    version="0.0.1",
+    author="Deshana Desai, Etai Shuchatowitz, Zhongshi Jiang, Teseo Schneider,Daniele Panozzo",
+    author_email="dkd266@nyu.edu",
+    description="An Automatic Generator for Gradients and Hessians ofC99 functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deshanadesai/autodiff",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
