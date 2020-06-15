f17d630b405c4ddee79a29e458814d0f089a83ef
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-06-15 06:49:03 +0100

    Fix imports

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="autodiff-deshanadesai", # Replace with your own username
-    version="0.0.1",
+    name="acorns",
+    version="0.0.2",
     author="Deshana Desai, Etai Shuchatowitz, Zhongshi Jiang, Teseo Schneider,Daniele Panozzo",
     author_email="dkd266@nyu.edu",
     description="An Automatic Generator for Gradients and Hessians ofC99 functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/deshanadesai/acorns",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests", "examples")),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    package_dir="src",
+    package_dir={"": "src"},
 )
