74e51dada730bf6d066cff42738a80067e7682cf
Deshana Desai <desai.deshna@gmail.com>
2020-06-15 20:08:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/deshanadesai/acorns",
-    packages=setuptools.find_packages(exclude=("tests", "examples"), where="acorns"),
+    packages=setuptools.find_packages(exclude=("tests", "examples")),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
